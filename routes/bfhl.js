74edd3b5ac7ebc02669b387bc28113a7532644fe
Samar-Lpu-sk <samarth.11911223@lpu.in>
2022-03-02 18:57:29 +0530

    task completed

---
+++ routes/bfhl.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const isNumber = require('../utils/Utils')
+const router = express.Router();
+
+
+router.post(
+    '/bfhl',
+    (req,res)=>{
+        var response = {
+            user_id:"samarth_gupta_19022002",
+            roll_number:"11911223",
+            numbers: [],
+            alphabets: [],
+            is_success: false
+        };
+        try{
+            const {data} = req.body;
+            if(!data){
+                throw new Error("data not found!");
+            }
+
+            const nums = [];
+            const alphabets = [];
+            for (const item of data){
+                if(isNumber(item)){
+                    nums.push(parseInt(item));
+                } else {
+                    alphabets.push(item);
+                }
+            }
+            response.numbers = nums;
+            response.alphabets = alphabets
+            response.is_success = true;
+            res.json(response);
+        }catch(err){
+            response.error = err.message;
+            res.status(500).json(response);
+        }
+    }
+)
+module.exports = router;
